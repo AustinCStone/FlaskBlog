eed83e9a664b6c1a89d6109b4433bb9ba5702989
Austin Stone <austinstone@utexas.edu>
2014-12-17 16:43:21 -0600

    fixed db_create

--- db_create.py
+++ db_create.py
@@ -1,21 +1,17 @@
-from app import db
-from models import BlogPost
-from models import User
+from project import db
+from project.models import BlogPost
+from project.models import User
 #create database
 db.create_all()
 
-from app import db
+from project import db
 # from models import BlogPost
 
 # create the database and the db table
 db.create_all()
 
 # insert data
-db.session.add(BlogPost("Good", "I\'m good.",1))
-db.session.add(BlogPost("Well", "I\'m well.",1))
-db.session.add(BlogPost("Excellent", "I\'m excellent.",1))
-db.session.add(BlogPost("Okay", "I\'m okay.",1))
-db.session.add(BlogPost("postgres", "we setup a local postgres instance",1))
+db.session.add(BlogPost("First Post", "First Post Content",1))
 db.session.add(User("admin","admin@admin.com","admin"))
 
 # commit the changes
